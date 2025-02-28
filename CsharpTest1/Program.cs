519aa1aefdb58c536d8bf53ce928af673a7c7495
mskang <siminlove1223@naver.com>
2025-02-28 23:49:06 +0900

    second test commit with macPP∏∏

--- CsharpTest1/Program.cs
+++ CsharpTest1/Program.cs
@@ -4,5 +4,6 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 Console.WriteLine("Test Git clone");
+Console.WriteLine("Commit Test");
 var host = builder.Build();
 host.Run();
