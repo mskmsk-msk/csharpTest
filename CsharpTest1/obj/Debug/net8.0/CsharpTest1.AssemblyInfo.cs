befbb3552047c44ad7766e219be172910130d7b2
mskang <siminlove1223@naver.com>
2025-03-01 20:33:55 +0900

    ㅔㄴㄴ

--- CsharpTest1/obj/Debug/net8.0/CsharpTest1.AssemblyInfo.cs
+++ CsharpTest1/obj/Debug/net8.0/CsharpTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519aa1aefdb58c536d8bf53ce928af673a7c7495")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
